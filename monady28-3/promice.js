e1d03b354f33d14c085e6712cb49530e65bd1505
unknown <dua.alsafasfeh@gmail.com>
2022-03-28 22:09:02 -0700

    task

--- monady28-3/promice.js
+++ monady28-3/promice.js
@@ -1,8 +1,20 @@
-let x=new Promis(function(resolve1 , reject1){
-if (document.getElementById("input").value=="true"){
+document.getElementById("submit").onclick = function ( ){
+
+let _promise = new Promise (function(resolve1 , reject1){
+if (document.getElementById("input").value =="Sure"){
     resolve1("it will a nice trip");
 }else{
-    reject1("you will ")
+    reject1("you will be dissappointed");
+ }
+});
+
+_promise.then( 
+    function (resolve1){
+        document.getElementById("result1").textContent = resolve1;
+    })
+    .catch(
+        function (reject1) {
+            document.getElementById ("result").textContent = reject1;
+        }
+    );
 }
-}
-);
